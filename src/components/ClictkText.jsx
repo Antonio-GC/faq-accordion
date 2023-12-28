f2f715683de30dd5a1c30b793e2d0267da6de3c5
Antonio-GC <jegonzalez.corporativo@gmail.com>
2023-12-28 11:45:14 -0500

    change position

--- src/components/ClictkText.jsx
+++ src/components/ClictkText.jsx
@@ -10,12 +10,12 @@ function ClickText({question,answer}) {
     <>
     <div className="grid my-3 gap-y-4">
           <div className='flex justify-between text-black gap-y-2'>
-      <a className="hover:text-[#AD28EB]" href="#" onClick={()=>setClickAnswer(!clickAnswer)}>{question}</a>
+      <a className="hover:text-[#AD28EB] font-bold" href="#" onClick={()=>setClickAnswer(!clickAnswer)}>{question}</a>
       <img src={clickAnswer ? urlPlus : urlMinus} alt="" />
       
     </div>
     <div>
-      <p className="text-[#8B6990] text-left">{clickAnswer ? answer : null }</p>
+      <p className="text-[#8B6990] text-left text-xs">{clickAnswer ? answer : null }</p>
       
     </div>
     </div>
